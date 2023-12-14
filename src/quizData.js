a980344ef3768312dc5560871c345b1737f8ffa1
JonBrown95 <126886504+JonBrown95@users.noreply.github.com>
2023-12-14 20:34:51 +0000

    Added friends questions data

---
+++ src/quizData.js
@@ -0,0 +1,64 @@
+const quizData = [
+    {
+      id: 1,
+      question: "What is the name of Ross' second wife?",
+      options: ["Emily", "Carol", "Janice", "Susan"],
+      answer: "Emily"
+    },
+    {
+      id: 2,
+      question: "What is the name of Joey's stuffed penguin?",
+      options: ["Hugsy", "Waddles", "Snowy", "Penny"],
+      answer: "Hugsy"
+    },
+    {
+      id: 3,
+      question: "Which character famously says 'How you doin'?'",
+      options: ["Joey", "Ross", "Chandler", "Monica"],
+      answer: "Joey"
+    },
+    {
+      id: 4,
+      question: "Where does Ross work?",
+      options: ["Central Perk", "Museum of Natural History", "Chandler's office", "BLOOMINGDALE'S"],
+      answer: "Museum of Natural History"
+    },
+    {
+      id: 5,
+      question: "What is the name of the coffee shop the friends frequently visit?",
+      options: ["Central Cafe", "Coffee Place", "Central Perk", "Perk Central"],
+      answer: "Central Perk"
+    },
+    {
+      id: 6,
+      question: "Which friend is a chef?",
+      options: ["Monica", "Rachel", "Phoebe", "Joey"],
+      answer: "Monica"
+    },
+    {
+      id: 7,
+      question: "What is the name of Ross and Monica's dog?",
+      options: ["Spot", "Fido", "Marcel", "Chi-Chi"],
+      answer: "Chi-Chi"
+    },
+    {
+      id: 8,
+      question: "Which character sings 'Smelly Cat'?",
+      options: ["Rachel", "Phoebe", "Monica", "Joey"],
+      answer: "Phoebe"
+    },
+    {
+      id: 9,
+      question: "Who gets stuck in a pair of leather pants?",
+      options: ["Ross", "Chandler", "Joey", "Monica"],
+      answer: "Ross"
+    },
+    {
+      id: 10,
+      question: "What is Joey's catchphrase?",
+      options: ["How you doin'?", "Smelly Cat", "We were on a break!", "Could I BE any more..."],
+      answer: "How you doin'?"
+    }
+  ];
+
+  export default quizData;
