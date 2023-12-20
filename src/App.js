e35fbbe2be22f2d51e026da15ebe0da1ecf0a175
JonBrown95 <126886504+JonBrown95@users.noreply.github.com>
2023-12-20 20:06:02 +0000

    Cleaned up App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Quiz from "./Components/Quiz";
 function App() {
   return (
     <div className="App">
-      <h1 className="font-bold text-3xl">Friends Quiz</h1>
+      
 
       <main>
         <Quiz />
