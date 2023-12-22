66d201532d46bcd106f32337e91013d68bd7bdc3
JonBrown95 <126886504+JonBrown95@users.noreply.github.com>
2023-12-22 21:08:00 +0000

    Styled quiz container

--- src/Components/Quiz.js
+++ src/Components/Quiz.js
@@ -53,42 +53,46 @@ const Quiz = () => {
   };
 
   return (
-    <div className="max-w-md mx-auto p-4 bg-gray-100 rounded-lg shadow-md">
-      {!quizStarted ? (
-        <div>
-          <p>Ready to start?</p>
-          <button className="bg-blue-500 text-white" onClick={startQuiz}>
-            Start Quiz
-          </button>
-        </div>
-      ) : !showResult ? (
-        <div>
-          <h2 className="text-lg font-semibold mb-4">
-          <p>Time Left: {timeLeft} seconds</p>
-            Question {currentQuestion + 1} -
-          </h2>
-          <h3 className="text-xl mb-4">{quizData[currentQuestion].question}</h3>
-          <div className="grid gap-3">
-            {quizData[currentQuestion].options.map((option, index) => (
-              <button
-                key={index}
-                className="bg-blue-400 text-white px-4 py-2 rounded-md hover:bg-blue-600"
-                onClick={() => handleAnswerClick(option)}
-              >
-                {option}
-              </button>
-            ))}
+    <div className="bg-gray-100 min-h-screen flex items-center justify-center">
+      <div className="w-[400px] h-[400px] p-6 bg-white rounded-lg shadow-md border border-gray-300 overflow-y-auto">
+        {!quizStarted ? (
+          <div>
+            <p>Ready to start?</p>
+            <button className="bg-blue-500 text-white" onClick={startQuiz}>
+              Start Quiz
+            </button>
           </div>
-        </div>
-      ) : (
-        <div>
-          <h2 className="text-lg font-semibold mb-4">Result</h2>
-          <p>Your score: {score}</p>
-          <button className="bg-blue-500 text-white " onClick={resetQuiz}>
-            Restart Quiz
-          </button>
-        </div>
-      )}
+        ) : !showResult ? (
+          <div>
+            <h2 className="text-lg font-semibold mb-4">
+              <p>Time Left: {timeLeft} seconds</p>
+              Question {currentQuestion + 1} -
+            </h2>
+            <h3 className="text-xl mb-4">
+              {quizData[currentQuestion].question}
+            </h3>
+            <div className="grid gap-3">
+              {quizData[currentQuestion].options.map((option, index) => (
+                <button
+                  key={index}
+                  className="bg-blue-400 text-white px-4 py-2 rounded-md hover:bg-blue-600"
+                  onClick={() => handleAnswerClick(option)}
+                >
+                  {option}
+                </button>
+              ))}
+            </div>
+          </div>
+        ) : (
+          <div>
+            <h2 className="text-lg font-semibold mb-4">Result</h2>
+            <p>Your score: {score}</p>
+            <button className="bg-blue-500 text-white " onClick={resetQuiz}>
+              Restart Quiz
+            </button>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
